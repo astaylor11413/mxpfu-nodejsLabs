a922529ae25260ef4085566db55b77c5eef66162
ashley <astaylor140@gmail.com>
2025-02-09 22:11:02 -0500

    Created end points for users

--- routes/users.js
+++ routes/users.js
@@ -25,35 +25,98 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    res.send(JSON.stringify({users},null,4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filteredUsers = users.filter((user)=>user.email===email);
+  if(filteredUsers.length>0){
+    res.send(filteredUsers[0]);
+  }else{
+    res.send("User could not be found");
+  }
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    users.push({
+        "firstName":req.query.firstName,
+        "lastName":req.query.lastName,
+        "email":req.query.email,
+        "DOB":req.query.DOB
+    });
+    res.send(users[(users.length)-1].firstName+" has been added!")
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    const email = req.params.email;
+    //retrieve user that needs to be updated
+    let filteredUsers = users.filter((user)=>user.email===email);
+
+    if(filteredUsers.length>0){
+        let filteredUser = filteredUsers[0];
+        //update user info depending on query request
+        if(req.query.firstName){
+            filteredUser.firstName = req.query.firstName;
+        }
+
+        if(req.query.lastName){
+            filteredUser.lastName = req.query.lastName;
+        }
+
+        if(req.query.email){
+            filteredUser.email = req.query.email;
+        }
+
+        if(req.query.DOB){
+            filteredUser.email = req.query.DOB;
+        }
+        //remove old user object and add updated user object
+        users = users.filter((user)=>user.email!==email);
+        users.push(filteredUser);
+        res.send(filteredUser.firstName+"'s profile has been updated");
+    }else{
+        res.send("User could not be found");
+      }
 });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+
+    users = users((user)=>user.email!==req.params.email);
+    res.send("The user with the following id has been deleted: "+ req.params.email);
+});
+
+//GET request: Retrieve all users with a particular last name
+router.get("/lastName/:lastName",(req,res)=>{
+    const lastName = req.params.lastName;
+    let filteredUsers = users.filter((user)=>user.lastName===lastName);
+    if(filteredUsers.length>0){
+        res.send(filteredUsers);
+    }else{
+        res.send("No users currently have that last name");
+    }
+});
+function getDate(strDate){
+    //DOB:"21-03-1989"
+    let dateArray = strDate.split('-');
+    return new Date(dateArray[2]+"/"+dateArray[1]+"/"+dateArray[0]);
+}
+
+//GET request: Retrieve users as a list sorted by date of birth
+router.get("/sort/DOB",(req,res)=>{
+    let sortedUsers = users.sort((userA,userB)=>{
+        let date1 = getDate(userA.DOB);
+        let date2 = getDate(userB.DOB);
+        return date1-date2;
+    });
+    res.send(sortedUsers);
 });
 
 module.exports=router;
